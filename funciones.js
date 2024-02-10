1fdd2b28da418782f884578138960454e53edafe
Jeferson Chacon <contacto@example>
2024-02-10 18:15:05 -0500

    video 9 curso GAS-avanzado

--- funciones.js
+++ funciones.js
@@ -1,10 +1,17 @@
 function doGet()
 {
-    return HtmlService.createTemplateFromFile("web").evaluate()
-                                                    .setTitle("Agenda Google Apps Script");
+    return HtmlService.createTemplateFromFile("web").evaluate().setTitle("Agenda Google Apps Script");
 }
 
 function obtenerDatosHTML(nombre)
 {
-    return HtmlService.createHtmlOutputFromFile(nombre).getContent();
+    return  HtmlService.createHtmlOutputFromFile(nombre).getContent();
+}
+
+//Funcion que toma los datos que existen en la hoja de excel y los guarda en un rango.
+//esta funcion sera llamada desde js.html desde la funcion crearTablaContactos()
+function obtenerContactos(){
+    let hoja = SpreaddheetApp.openById("1TW8eC6RJnifsaU5tojnxx7MHjJ5_QLBef5EwMqh0QWQ").getActiveSheet();
+    let datos = hoja.getDataRange().getValues();
+    return datos;
 }
