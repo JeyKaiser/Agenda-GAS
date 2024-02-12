43cef77741b905bdfb31653925a33554ec6b8b89
Jeferson Chacon <contacto@example>
2024-02-11 23:38:33 -0500

    video 15 curso GAS avanzado1

--- funciones.js
+++ funciones.js
@@ -1,4 +1,11 @@
+const HOJA = SpreadsheetApp.openById('1TW8eC6RJnifsaU5tojnxx7MHjJ5_QLBef5EwMqh0QWQ').getActiveSheet();
+
 function doGet(){
+    return HtmlService.createTemplateFromFile("web").evaluate().setTitle("Agenda Google Apps Script");
+}
+
+function doPost(datos){
+    insertarContactos(datos.parameter.nombre, datos.parameter.correo);
     return HtmlService.createTemplateFromFile("web").evaluate().setTitle("Agenda Google Apps Script");
 }
 
@@ -9,8 +16,10 @@ function obtenerDatosHTML(nombre){
 //Funcion que toma los datos que existen en la hoja de excel y los guarda en un rango.
 //esta funcion sera llamada desde js.html desde la funcion crearTablaContactos()
 function obtenerContactos(){
-    let hoja = SpreadsheetApp.openById("1TW8eC6RJnifsaU5tojnxx7MHjJ5_QLBef5EwMqh0QWQ").getActiveSheet();
-    let datos = hoja.getDataRange().getValues();
-    //Logger.log(datos);
-    return datos;
+    return HOJA.getDataRange().getValues();
+    //Logger.log(datos);    
+}
+
+function insertarContactos(nombre, correo){
+    HOJA.appendRow([nombre,correo]);
 }
